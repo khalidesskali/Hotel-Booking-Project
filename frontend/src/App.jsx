9e5d639b96c99547a7f5c6bb69beb839857b2ff9
khalidesskali <esskalikhalid7@gmail.com>
2024-11-23 22:03:09 +0100

    Routing The Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./components/Home";
+import Rooms from "./components/Rooms";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
