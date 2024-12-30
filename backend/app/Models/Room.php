b281f53ec8dc418a064f1b9afe56287d82f028f6
khalidesskali <esskalikhalid7@gmail.com>
2024-12-30 12:44:32 +0100

    Add special offers table and link the models

--- backend/app/Models/Room.php
+++ backend/app/Models/Room.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
     use HasFactory;
+
+    public function bookings () {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function specialOffers () {
+        return $this->hasMany(SpecialOffer::class);
+    }
 }
