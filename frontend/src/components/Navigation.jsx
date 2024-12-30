bd9df81b44a2c3c87add4eb2b391c62f0c92d1a2
khalidesskali <esskalikhalid7@gmail.com>
2024-12-30 18:12:11 +0100

    Set Nav on Mobile first principle design

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="flex items-center justify-between gap-80">
-      <ul className="links flex items-center font-medium">
-        <li>
-          <Link to="/rooms">Our Rooms</Link>
-        </li>
-        <li>
-          <Link to="/facilities">Facilities</Link>
-        </li>
-        <li>
-          <Link to="about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Us</Link>
-        </li>
-      </ul>
-      <div className="auth flex gap-5">
-        <Button className="w-24">
-          <Link to="/login">Login</Link>
-        </Button>
-        <Button className="w-24">
-          <Link to="/signup">Signup</Link>
-        </Button>
+    <nav>
+      <div
+        className="nav flex justify-between flex-col bg-gray-100 fixed top-0 h-screen w-full max-w-80
+          py-20 px-7 gap-5 xl:gap-80 transition-all duration-300"
+        style={{ right: isOpen ? "0" : "-350px" }}
+      >
+        <ul className="links mt-8 flex-col flex  font-medium gap-6">
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+          <li>
+            <Link to="/offers">Offers</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="auth flex justify-center gap-8">
+          <Button className="w-24">
+            <Link to="/login">Login</Link>
+          </Button>
+          <Button className="w-24">
+            <Link to="/signup">Signup</Link>
+          </Button>
+        </div>
       </div>
+      <button
+        className="relative flex items-center justify-center text-gray-800"
+        style={{ fontSize: "1.4rem" }}
+        onClick={toggleMenu}
+      >
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </button>
     </nav>
   );
 };
