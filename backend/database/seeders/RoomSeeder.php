aea165df6835bc8f0940b24b084cb506f053627c
khalidesskali <esskalikhalid7@gmail.com>
2024-12-31 12:37:57 +0100

    Add rooms to the database

--- backend/database/seeders/RoomSeeder.php
+++ backend/database/seeders/RoomSeeder.php
@@ -11,60 +11,188 @@ class RoomSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        Room::create([
-            'room_number' => '101',
-            'image' => '../src/images/deluxe-suite.jpg',
-            'room_type' => 'Deluxe Suite',
-            'price_per_night' => 250,
-            'description' => 'A luxurious suite with ocean view, king-size bed, and private balcony'
-        ]);
+        // Room::create([
+        //     'room_number' => '101',
+        //     'image' => '../src/images/deluxe-suite.jpg',
+        //     'room_type' => 'Deluxe Suite',
+        //     'price_per_night' => 250,
+        //     'description' => 'A luxurious suite with ocean view, king-size bed, and private balcony'
+        // ]);
 
-        Room::create([
-            'room_number' => '102',
-            'image' => '../src/images/standard-room.jpg',
-            'room_type' => 'Standard Room',
-            'price_per_night' => 250,
-            'description' => 'A comfortable room with all basic amenities for a pleasant stay'
-        ]);
+        // Room::create([
+        //     'room_number' => '102',
+        //     'image' => '../src/images/standard-room.jpg',
+        //     'room_type' => 'Standard Room',
+        //     'price_per_night' => 250,
+        //     'description' => 'A comfortable room with all basic amenities for a pleasant stay'
+        // ]);
 
-        Room::create([
-            'room_number' => '103',
-            'image' => '../src/images/family-room.jpg',
-            'room_type' => 'Family Room',
-            'price_per_night' => 180,
-            'description' => 'Spacious room perfect for families with kids, includes bunk beds and a play are'
-        ]);
+        // Room::create([
+        //     'room_number' => '103',
+        //     'image' => '../src/images/family-room.jpg',
+        //     'room_type' => 'Family Room',
+        //     'price_per_night' => 180,
+        //     'description' => 'Spacious room perfect for families with kids, includes bunk beds and a play are'
+        // ]);
 
-        Room::create([
-            'room_number' => '104',
-            'image' => '../src/images/Penthouse-Suite.jpeg',
-            'room_type' => 'Presidential Suite',
-            'price_per_night' => 500,
-            'description' => 'An opulent suite with a private jacuzzi, personal butler, and stunning views'
-        ]);
+        // Room::create([
+        //     'room_number' => '104',
+        //     'image' => '../src/images/Penthouse-Suite.jpeg',
+        //     'room_type' => 'Presidential Suite',
+        //     'price_per_night' => 500,
+        //     'description' => 'An opulent suite with a private jacuzzi, personal butler, and stunning views'
+        // ]);
 
-        Room::create([
-            'room_number' => '105',
-            'image' => '../src/images/single-room.webp',
-            'room_type' => 'Single Room',
-            'price_per_night' => 90,
-            'description' => 'A cozy single room ideal for solo travelers'
-        ]);
+        // Room::create([
+        //     'room_number' => '105',
+        //     'image' => '../src/images/single-room.webp',
+        //     'room_type' => 'Single Room',
+        //     'price_per_night' => 90,
+        //     'description' => 'A cozy single room ideal for solo travelers'
+        // ]);
 
-        Room::create([
-            'room_number' => '106',
-            'image' => '../src/images/double-room.jpg',
-            'room_type' => 'Double Room',
-            'price_per_night' => 140,
-            'description' => 'Perfect for couples, featuring a queen-size bed and city view'
-        ]);
+        // Room::create([
+        //     'room_number' => '106',
+        //     'image' => '../src/images/double-room.jpg',
+        //     'room_type' => 'Double Room',
+        //     'price_per_night' => 140,
+        //     'description' => 'Perfect for couples, featuring a queen-size bed and city view'
+        // ]);
 
-        Room::create([
-            'room_number' => '107',
-            'image' => '../src/images/Penthouse-Suite.jpeg',
-            'room_type' => 'Penthouse Suite',
-            'price_per_night' => 700,
-            'description' => 'A luxurious top-floor suite with private elevator access and stunning panoramic views'
-        ]);
+        // Room::create([
+        //     'room_number' => '107',
+        //     'image' => '../src/images/Penthouse-Suite.jpeg',
+        //     'room_type' => 'Penthouse Suite',
+        //     'price_per_night' => 700,
+        //     'description' => 'A luxurious top-floor suite with private elevator access and stunning panoramic views'
+        // ]);
+
+        // Room::create([
+        //     'room_number' => '108',
+        //     'image' => '../src/images/standard_room_1.jpg',
+        //     'room_type' => 'Serene Standard Room',
+        //     'price_per_night' => 120,
+        //     'description' => 'Unwind in a peaceful ambiance designed for relaxation, featuring cozy furnishings and calming décor'
+        // ]);
+
+        // Room::create([
+        //     'room_number' => '109',
+        //     'image' => '../src/images/double-room-1.webp',
+        //     'room_type' => 'Romantic Double Retreat',
+        //     'price_per_night' => 150,
+        //     'description' => 'A perfect escape for couples, featuring a cozy bed, elegant décor, and a warm ambiance'
+        // ]);
+
+        // Room::create([
+        //     'room_number' => '110',
+        //     'image' => '../src/images/suite-room-1.jpg',
+        //     'room_type' => 'Prestige Suite',
+        //     'price_per_night' => 250,
+        //     'description' => 'A luxurious space with separate living and sleeping areas, perfect for an indulgent getaway.'
+        // ]);
+
+        // Room::create([
+        //     'room_number' => '111',
+        //     'image' => '../src/images/family-room-1.jpg',
+        //     'room_type' => 'Cozy Family Haven',
+        //     'price_per_night' => 180,
+        //     'description' => 'A spacious room perfect for families, featuring multiple beds and kid-friendly amenities'
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '112',
+        //     'image' => '../src/images/family-room-2.jpg',
+        //     'room_type' => 'Family Comfort Retreat',
+        //     'price_per_night' => 200,
+        //     'description' => 'Designed for family fun and relaxation, this room offers ample space and modern conveniences',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '113',
+        //     'image' => '../src/images/double-room-2.jpg',
+        //     'room_type' => 'Double Comfort Oasis',
+        //     'price_per_night' => 125,
+        //     'description' => 'A spacious room designed for two, offering comfort, privacy, and modern amenities',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '114',
+        //     'image' => '../src/images/suite-room-2.jpg',
+        //     'room_type' => 'Opulent Suite Escape',
+        //     'price_per_night' => 280,
+        //     'description' => 'Immerse yourself in sophistication with plush furnishings and premium amenities',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '115',
+        //     'image' => '../src/images/standard_room_2.webp',
+        //     'room_type' => 'Standard Haven of Comfort',
+        //     'price_per_night' => 105,
+        //     'description' => 'Experience ultimate comfort with a plush bed, modern amenities, and an inviting atmosphere',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '116',
+        //     'image' => '../src/images/family-room-3.webp',
+        //     'room_type' => 'The Family Getaway Nest',
+        //     'price_per_night' => 220,
+        //     'description' => 'A warm and inviting space with thoughtful touches to make your family vacation special',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '117',
+        //     'image' => '../src/images/double-room-3.jpg',
+        //     'room_type' => 'Luxury Double Delight',
+        //     'price_per_night' => 160,
+        //     'description' => 'A well-appointed room with refined furnishings and thoughtful touches for a memorable stay',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '118',
+        //     'image' => '../src/images/suite-room-3.webp',
+        //     'room_type' => 'Signature Suite Bliss',
+        //     'price_per_night' => 300,
+        //     'description' => 'A spacious suite offering exquisite décor and unparalleled comfort for discerning travelers',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '119',
+        //     'image' => '../src/images/standard_room_3.jpg',
+        //     'room_type' => 'Elegant Standard Retreat',
+        //     'price_per_night' => 130,
+        //     'description' => 'Stay in style with a refined design that balances elegance and functionality, perfect for any traveler',
+        // ]);
+
+        // Room::create([
+        //     'room_number' => '120',
+        //     'image' => '../src/images/family-room-4.webp',
+        //     'room_type' => 'Classic Family Suite',
+        //     'price_per_night' => 195,
+        //     'description' => 'Enjoy a blend of comfort and practicality with plenty of room for everyone to unwind',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '121',
+        //     'image' => '../src/images/double-room-4.webp',
+        //     'room_type' => 'Classic Double Serenity',
+        //     'price_per_night' => 135,
+        //     'description' => 'Unwind in a serene setting with two perfectly arranged beds for your convenience',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '122',
+        //     'image' => '../src/images/suite-room-4.jpg',
+        //     'room_type' => 'Elegant Suite Retreat',
+        //     'price_per_night' => 260,
+        //     'description' => 'Relax in style with a beautifully designed suite, ideal for both leisure and business stays',
+        // ]);
+        
+        // Room::create([
+        //     'room_number' => '123',
+        //     'image' => '../src/images/standard-room-4.jpeg',
+        //     'room_type' => 'Luxe Standard Escape',
+        //     'price_per_night' => 140,
+        //     'description' => 'A comfortable and sophisticated space with modern amenities for your enjoyment',
+        // ]);
     }
 }
