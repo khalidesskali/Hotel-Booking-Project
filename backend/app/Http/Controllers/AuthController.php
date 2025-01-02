e0c6bf15698a6960bc9cc6a2bbfd7cbf8b93727a
khalidesskali <esskalikhalid7@gmail.com>
2025-01-02 21:36:15 +0100

    Set up and create auth controller with their API's

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!Auth::attempt($validatedData)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(compact('user', 'token'), 200);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'User logged out successfully']);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,19 @@
 use App\Http\Controllers\RoomsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\AuthController;
 
+// Signup Route
 Route::post('/signup', [SignupController::class, 'store']);
 
+// Login Route
+Route::post('/login', [AuthController::class, 'login']);
+
+// Authenticated Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Rooms Route
 Route::get('/rooms', [RoomsController::class, 'rooms']);
