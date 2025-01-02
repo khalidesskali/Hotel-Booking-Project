6c6d94409fb28015609354c1e40bb90d50f76aac
khalidesskali <esskalikhalid7@gmail.com>
2025-01-02 21:39:17 +0100

    adjust react router best practice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { Routes, Route } from "react-router";
 import Home from "./components/Home";
 import Rooms from "./components/Rooms";
 import About from "./components/About";
@@ -14,22 +14,20 @@ import Book from "./components/Book";
 const App = () => {
   return (
     <div>
-      <Router>
-        <Header />
-        <div className="mt-16">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rooms" element={<Rooms />} />
-            <Route path="/rooms/:id" element={<Book />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/offers" element={<Offers />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/*" element={<PageNotFound />} />
-          </Routes>
-        </div>
-      </Router>
+      <Header />
+      <div className="mt-16">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/rooms/:id" element={<Book />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </div>
     </div>
   );
 };
